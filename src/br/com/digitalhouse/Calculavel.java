42572aebae41865cf3539d1c5fada4b7a37fb1e3
sergioitazar <itazar@gmail.com>
2019-04-12 13:28:45 -0300

    Initial commit

---
+++ src/br/com/digitalhouse/Calculavel.java
@@ -0,0 +1,6 @@
+package br.com.digitalhouse;
+
+public interface Calculavel {
+    void calcular();
+
+}
---
+++ src/br/com/digitalhouse/Principal.java
@@ -0,0 +1,7 @@
+package br.com.digitalhouse;
+
+public class Principal {
+    public static void main(String[] args){
+// construtor
+
+    }
